55a00a2d4d061424a5f7df4dc414df48f5db67dd
Marko50 <fernandoandre49@gmail.com>
2018-05-17 17:24:00 +0100

    Genetic algortithm corrected, next step is simmulated annealing

--- Projeto/Logic/__main__.py
+++ Projeto/Logic/__main__.py
@@ -1,78 +1,8 @@
-# coding=utf-8
-# B2: Otimizacao na Organizacao de um Jantar
-#
-# Objectivo
-#
-# Resolucao do problema de optimizacao da distribuicao de pessoas por mesas num jantar.
-#
-# Descricao
-#
-# Esta a ser organizado um jantar solidario, no qual se registaram centenas de pessoas.
-#
-# Muitas delas estao registadas em grupo (de cônjuges, famílias, amigos), pelo que têm que ficar sentadas na mesma mesa.
-# Por outro lado, pretende-se que as pessoas se sintam confortaveis com a companhia que vao ter durante o jantar,
-# pelo que devem ser distribuídas de forma a terem alguma afinidade (etaria, profissional, hobística, etc).
-#
-# Cada mesa podera ter entre Min e Max lugares.
-# A sala onde decorrera o jantar comporta um maximo de Nt mesas de cada tamanho t (número de lugares).
-# Pretende-se saber quantas mesas de cada tamanho devem ser utilizadas, e como deverao ficar distribuídas as pessoas.
-#
-# Na resolucao deste problema pode utilizar múltiplas metodologias de optimizacao (Algoritmos Geneticos e uma delas).
-# Estas metodologias podem ser utilizadas em conjunto de forma a obter uma solucao de boa qualidade para o problema
-# (por exemplo criando uma solucao inicial que depois e iterativamente melhorada).
-# Ou podem ser simplesmente utilizadas para efectuar uma analise comparativa do nível de desempenho de cada metodologia.
-#
-# e tambem valorizada a modelizacao realista do problema.
-#-Representacao do tema como problema de otimizacao: estados, funcao de cruzamento/mutacao,
-# funcao de vizinhanca, funcao de avaliacao, criterios de paragem.
-# Algoritmos de otimizacao a aplicar (ilustrados para o caso concreto).
 
 
 
-from deap import base,tools,creator
-import sys
-from Logic.genetic import *
-sys.path.append("..")
-from FileParser import ParseFile
 
 
-filename = 'input'
-tuple = ParseFile.parseFile(filename)
-people = tuple[0]
-tables = tuple[1]
-
-
-
-IND_SIZE = 50
-
-creator.create("FitMax", base.Fitness, weights=(1.0,))
-
-toolbox = base.Toolbox()
-toolbox.register("individual", generateDinner, tables, people)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual, n = IND_SIZE)
-
-toolbox.register("mate", mate)
-toolbox.register("mutate", mutate)
-toolbox.register("select", select)
-
-
-
-pop = toolbox.population()
-counter = 0
-old_d = 0
-response = 0
-while (counter < 1000):
-    selected = toolbox.select(pop)
-    toolbox.mate(selected)
-    toolbox.mutate(selected)
-    pop = selected
-    d = max(pop)
-    counter = counter +1
-    if(d.totalAfinity() > old_d):
-        response = d
-        old_d = d.totalAfinity()
-
-print(response)
 
 
 
--- Projeto/Logic/genetic.py
+++ Projeto/Logic/genetic.py
@@ -1,8 +1,87 @@
+# coding=utf-8
+# B2: Otimizacao na Organizacao de um Jantar
+#
+# Objectivo
+#
+# Resolucao do problema de optimizacao da distribuicao de pessoas por mesas num jantar.
+#
+# Descricao
+#
+# Esta a ser organizado um jantar solidario, no qual se registaram centenas de pessoas.
+#
+# Muitas delas estao registadas em grupo (de cônjuges, famílias, amigos), pelo que têm que ficar sentadas na mesma mesa.
+# Por outro lado, pretende-se que as pessoas se sintam confortaveis com a companhia que vao ter durante o jantar,
+# pelo que devem ser distribuídas de forma a terem alguma afinidade (etaria, profissional, hobística, etc).
+#
+# Cada mesa podera ter entre Min e Max lugares.
+# A sala onde decorrera o jantar comporta um maximo de Nt mesas de cada tamanho t (número de lugares).
+# Pretende-se saber quantas mesas de cada tamanho devem ser utilizadas, e como deverao ficar distribuídas as pessoas.
+#
+# Na resolucao deste problema pode utilizar múltiplas metodologias de optimizacao (Algoritmos Geneticos e uma delas).
+# Estas metodologias podem ser utilizadas em conjunto de forma a obter uma solucao de boa qualidade para o problema
+# (por exemplo criando uma solucao inicial que depois e iterativamente melhorada).
+# Ou podem ser simplesmente utilizadas para efectuar uma analise comparativa do nível de desempenho de cada metodologia.
+#
+# e tambem valorizada a modelizacao realista do problema.
+#-Representacao do tema como problema de otimizacao: estados, funcao de cruzamento/mutacao,
+# funcao de vizinhanca, funcao de avaliacao, criterios de paragem.
+# Algoritmos de otimizacao a aplicar (ilustrados para o caso concreto).
+
+
+from deap import base,tools,creator
+import sys
+from Logic.genetic import *
+sys.path.append("..")
+from FileParser import ParseFile
+
 from random import shuffle,random
 from Classes.Dinner import Dinner
 from Classes.Person import Person
 import copy
+import operator
 
+
+
+
+def geneticAlgorithm():
+    filename = 'input'
+    tuple = ParseFile.parseFile(filename)
+    people = tuple[0]
+    tables = tuple[1]
+    IND_SIZE = 50
+
+    creator.create("FitMax", base.Fitness, weights=(1.0,))
+
+    toolbox = base.Toolbox()
+    toolbox.register("individual", generateDinner, tables, people)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual, n=IND_SIZE)
+
+    toolbox.register("mate", mate)
+    toolbox.register("mutate", mutate)
+    toolbox.register("select", select)
+
+    pop = toolbox.population()
+    counter = 0
+    old_d = 0
+    d = 0
+    next_it = 1
+    while (next_it):
+        selected = toolbox.select(pop)
+        toolbox.mate(selected)
+        toolbox.mutate(selected)
+        pop = selected
+        d = max(pop)
+        if (d.totalAfinity() > old_d):
+            next_it = 1
+            counter = 0
+            old_d = d.totalAfinity()
+        else:
+            old_d = d.totalAfinity()
+            counter = counter + 1
+            if (counter == 10):
+                next_it = 0
+
+    print(d)
 
 def generatePop(tables, people):
     peopleCopy = people[:]
@@ -55,10 +134,14 @@ def select(population):
         inc = population[y].setProb(total,acum)
         acum += inc
 
-    for a in range(0, l):
-        r = random()
+    #elitist selection
+    sorted_x = sorted(population, key=operator.attrgetter('afinity'))
+    selectedPop[0] = sorted_x[len(sorted_x) - 1]
+    selectedPop[1] = sorted_x[len(sorted_x) - 2]
 
-        for z in range(0, l):
+    for a in range(2, l):
+        r = random()
+        for z in range(0, l): ##probabilistic selection
             if (population[z].probabilityMin < r and population[z].probabilityMax >= r):
                 selectedPop[a] = copy.copy(population[z])
                 break
